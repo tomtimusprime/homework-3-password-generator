3ad45e71e90d7eed4df726f85a2a70f5d67a8243
Tom Black <tom.black06007@gmail.com>
2020-03-15 10:46:36 -0700

    Worked on the function to take user input on what type of characters they want to use for the password generator.

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
 //Declare variables
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
-let upperCase = lowerCase.map(function(letter) {
+let upperCase = lowerCase.map(function (letter) {
   return letter.toUpperCase();
-}) 
+})
 let special = "!@#$%^&*()-_+=?/\[]{}<>".split("");
-let length = 0;
-let useNumbers = false;
-let useLowerCase = false;
-let useUpperCase = false;
-let useSpecial = false;
-
-promptForLength();
+let length = promptForLength();
+let useNumbers = promptForCharacters("Do you want to use numbers?");
+let useLowerCase = promptForCharacters("Do you want to use lower case letters?");
+let useUpperCase = promptForCharacters("Do you want to use upper case letters?");
+let useSpecial = promptForCharacters("Do you want to use special characters?");
 
 function promptForLength() {
-  let userResponse = prompt("How long would you like your password to be? (No more than 128 characters)");
-  let numberResponse = Number(userResponse);
-  console.log(numberResponse);
-  if(isNaN(numberResponse)) {
-      userResponse = prompt("That's not a number. Please enter a number.");
-    } 
-    else {
-      if (numberResponse < 128 && numberResponse > 0) {
-        // console.log("valid input.");
-        length = numberResponse;
+  let numberResponse;
+  let userResponse;
+
+    do {
+      if (numberResponse === undefined) {
+        userResponse = prompt("How long would you like your password to be? (No more than 128 characters)");
       }
-      // console.log("is number.");
-  }
+      else if (isNaN(numberResponse)) {
+        userResponse = prompt("That's not a number. Please enter a number.")
+      }
+      else if(numberResponse > 128) {
+        userResponse = prompt("That's too many characters.");
+      }
+      else if (numberResponse < 8) {
+        userResponse = prompt("That's not enough characters.")
+      }
+      numberResponse = Number(userResponse);
+  } while (isNaN(numberResponse) || numberResponse > 128 || numberResponse < 8);
+  return numberResponse;   
+}
+
+function promptForCharacters(question) {
+  let response = prompt(question);
+  response.toLowerCase();
+  do {
+    if(response === "yes") {
+      return true;
+    }
+    else if (response === "no") {
+      return false;
+    }
+    else {
+      response = prompt("I don't recognize that answer. " + question);
+    }
+  } while (response !== "yes" || response !== "no"); // could also be "while(true);""
+ 
 }
 
 // Assignment Code
@@ -66,32 +87,32 @@ generateBtn.addEventListener("click", writePassword);
  * for loop based on number user gives us and then generate a random character on each iteration.
  * Use random number to selet a random character from our array and then push that to our password variable
  * Display on the generated password on document.
- * 
+ *
  */
 
 
 
 
- /**
-  * option + arrow key - moves the line up and down
-  * option + shift + arrow key - makes a copy of that current line
-  * command + enter - puts a new line below
-  * command + shift + enter - puts a new line above
-  * 
-  * playlist on higher order functions - youtube - fun fun function 
-  * Higher order functions take a function as an argument.
-  * 
-  * All of these higher order functions - they take a function as an argument and make copies of the arrays (except forEach).
-  * map(); makes a new array.
-  * filter();
-  * reduce(); 
-  * forEach(); - same as map function, but changes the original way
-  * 
-  * 
-  * var uses something called hoisting - 
-  * let and const don't get hoisted
-  * Let can be reassigned - if you want to have a variable that can change along the ways.
-  * Const cannot be reassigned - if there's a value should not be changed and you don't want it accidentally get changed. 
-  * 
-  * every function should be broken down as small as possible and do only one thing - the single purpose principle.
-  */
+/**
+ * option + arrow key - moves the line up and down
+ * option + shift + arrow key - makes a copy of that current line
+ * command + enter - puts a new line below
+ * command + shift + enter - puts a new line above
+ *
+ * playlist on higher order functions - youtube - fun fun function
+ * Higher order functions take a function as an argument.
+ *
+ * All of these higher order functions - they take a function as an argument and make copies of the arrays (except forEach).
+ * map(); makes a new array.
+ * filter();
+ * reduce();
+ * forEach(); - same as map function, but changes the original way
+ *
+ *
+ * var uses something called hoisting -
+ * let and const don't get hoisted
+ * Let can be reassigned - if you want to have a variable that can change along the ways.
+ * Const cannot be reassigned - if there's a value should not be changed and you don't want it accidentally get changed.
+ *
+ * every function should be broken down as small as possible and do only one thing - the single purpose principle.
+ */
