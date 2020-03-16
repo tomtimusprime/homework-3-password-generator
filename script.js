61cd172077a16080b414401ba55e3659c79d9854
Tom Black <tom.black06007@gmail.com>
2020-03-15 17:25:37 -0700

    Finished the function to be able to generate the password. Still need to make a few tweaks.

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
 //Declare variables
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 let upperCase = lowerCase.map(function (letter) {
   return letter.toUpperCase();
 })
-let special = "!@#$%^&*()-_+=?/\[]{}<>".split("");
-let length = promptForLength();
-let useNumbers = promptForCharacters("Do you want to use numbers?");
-let useLowerCase = promptForCharacters("Do you want to use lower case letters?");
-let useUpperCase = promptForCharacters("Do you want to use upper case letters?");
-let useSpecial = promptForCharacters("Do you want to use special characters?");
+let special = "!@#$%^&*()-_+=?/\[]{}<>".split(""); 
 
 function promptForLength() {
-  let numberResponse;
-  let userResponse;
-
-    do {
-      if (numberResponse === undefined) {
-        userResponse = prompt("How long would you like your password to be? (No more than 128 characters)");
-      }
-      else if (isNaN(numberResponse)) {
-        userResponse = prompt("That's not a number. Please enter a number.")
+  let userResponse = prompt("How long would you like your password to be? (No more than 128 characters)");;
+  let numberResponse = Number(userResponse);
+  while (isNaN(numberResponse) || numberResponse > 128 || numberResponse < 8) {
+      if (isNaN(numberResponse)) {
+        userResponse = prompt("That's not a number. Please enter a number.");
       }
       else if(numberResponse > 128) {
         userResponse = prompt("That's too many characters.");
       }
       else if (numberResponse < 8) {
-        userResponse = prompt("That's not enough characters.")
+        userResponse = prompt("That's not enough characters.");
       }
-      numberResponse = Number(userResponse);
-  } while (isNaN(numberResponse) || numberResponse > 128 || numberResponse < 8);
+      numberResponse = Number(userResponse);//Need to turn that value into a number once again because 
+  } 
   return numberResponse;   
 }
 
 function promptForCharacters(question) {
-  let response = prompt(question);
-  response.toLowerCase();
+  let response = prompt(question).toLowerCase();
   do {
     if(response === "yes") {
       return true;
@@ -45,13 +35,57 @@ function promptForCharacters(question) {
     }
     else {
       response = prompt("I don't recognize that answer. " + question);
+      response = response.toLowerCase();
     }
-  } while (response !== "yes" || response !== "no"); // could also be "while(true);""
- 
+  } while (response !== "yes" && response !== "no"); // could also be "while(true);""
+
 }
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// function promptForCharacters(question) {
+//   let response = prompt(question).toLowerCase();
+//   while(response !== "yes" && response !== "no") {
+//     response = prompt(question).toLowerCase();
+//   }
+//   return (response === "yes") ? true : false; 
+// }
+
+function generatePassword() {
+  let length = promptForLength();
+  let useNumbers = false;
+  let useLowerCase = false;
+  let useUpperCase = false;
+  let useSpecial = false;
+  
+  do {
+    useNumbers = promptForCharacters("Do you want to use numbers?");
+    useLowerCase = promptForCharacters("Do you want to use lower case letters?");
+    useUpperCase = promptForCharacters("Do you want to use upper case letters?");
+    useSpecial = promptForCharacters("Do you want to use special characters?");
+    if(!useNumbers && !useLowerCase && !useUpperCase && !useSpecial) {
+      alert("You need to choose at least one character type.");
+    }
+  } while(!useNumbers && !useLowerCase && !useUpperCase && !useSpecial);
+  
+  let passArray = [];//resets
+  if (useNumbers) {
+    passArray.splice(0,0,...numbers);
+  } 
+  if (useLowerCase) {
+    passArray.splice(0,0,...lowerCase);
+  }
+  if (useUpperCase) {
+    passArray.splice(0,0,...upperCase);
+  }
+  if (useSpecial) {
+    passArray.splice(0,0,...special);
+  }
+  let password = "";
+  for(i = 0; i <= length; i++) {
+    let randomNum = Math.floor(Math.random() * passArray.length);
+    password += passArray[randomNum];
+  }
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -59,19 +93,15 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-function generatePassword() {
-  //Get user input of long they would like their password to be.
-  let howLong = prompt("How long would you like your password to be? (No more than 128 characters)");
-  //Get user input of what character types they would like it to have
-  let charTypes = prompt("What character types would you like your password to have? Please specify lowercase, uppercase, numeric, and/or special characters.");
-
-}
-
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//If you want to get out of the number by clicking cancel how does that work?
+//If you want reset the password to the default text how would you do that?
 
 
 /** PSEUDO CODE
@@ -98,6 +128,10 @@ generateBtn.addEventListener("click", writePassword);
  * option + shift + arrow key - makes a copy of that current line
  * command + enter - puts a new line below
  * command + shift + enter - puts a new line above
+ * alt + command + up or down arrow - adds cursors vertically
+ * command + brackets - if you have a cursor in the middle of the line you can indent that line
+ * command + left or right arrow - shifts to the front of the line.
+ * command + shift + k - deletes current line wherever you are.
  *
  * playlist on higher order functions - youtube - fun fun function
  * Higher order functions take a function as an argument.
